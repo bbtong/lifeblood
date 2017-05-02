c89dd01a9d274f236b8454b641f8728304125f2e
Bryan Tong <btong@berkeley.edu>
2017-05-02 14:29:54 -0700

    Solid UI progress, database works for signing up

--- Lifeblood/SignupViewController.swift
+++ Lifeblood/SignupViewController.swift
@@ -58,8 +58,11 @@ class SignupViewController: UIViewController, UITextFieldDelegate, UIPickerViewD
     @IBAction func didAttemptSignup(_ sender: UIButton) {
         guard let name = nameField.text else { return }
         guard let age = ageField.text else { return } // replace with AGE VERIFIER
-        //guard let bloodType = bloodTypeField[selectedRow(inComponent: <#T##Int#>] else { return } // REPLACE WITH DROP DOWN MENU
-        guard let location = locationField.text else { return } // REPLACE WITH GOOGLE MAPS API OR ZIP CODE SYSTEM
+        let bloodType = bloodTypes[bloodTypeField.selectedRow(inComponent: 0)]
+        let purpose = purposeField.isEnabledForSegment(at: 0) // true is donating, false is receiving
+        guard let location = Int(locationField.text!) else { return } // REPLACE WITH GOOGLE MAPS API OR ZIP CODE SYSTEM
+        let userMaxLocation = location + 500
+        let userMinLocation = location - 500
         guard let email = emailField.text else { return }
         guard let password = passwordField.text else { return }
         if (Int(age)! < 18) {
@@ -75,8 +78,13 @@ class SignupViewController: UIViewController, UITextFieldDelegate, UIPickerViewD
             self.present(passAlert, animated: true, completion: nil)
         }
         // My sign up code below
-        let createdUser = FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
-            if let error = error {
+        _ = FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+            /*user!.name = name
+            user!.age = age
+            user!.maxLocation = userMaxLocation
+            user!.minLocation = userMinLocation
+            user!.blood = bloodType*/
+            if error != nil {
                 // If fails, display an alert message to try login again.
                 let alert = UIAlertController(title: "Sign-up Failed", message: "Signing up failed, please try again.", preferredStyle: UIAlertControllerStyle.alert)
                 alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
@@ -93,6 +101,19 @@ class SignupViewController: UIViewController, UITextFieldDelegate, UIPickerViewD
                 })
             }
         } )
+        let ref = FIRDatabase.database().reference()
+        let rootChild = ref.child("users")
+        /* Borrowed from Stack Overflow user William Hu. */
+        let uidChild = email.lowercased().replacingOccurrences(of: ".", with: ",", options: .literal, range: nil)
+        let userChild = rootChild.child(uidChild)
+        userChild.child("name").setValue(name)
+        userChild.child("age").setValue(age)
+        userChild.child("zipcode").setValue(location)
+        userChild.child("maxZip").setValue(userMaxLocation)
+        userChild.child("minZip").setValue(userMinLocation)
+        userChild.child("bloodType").setValue(bloodType)
+        userChild.child("purpose").setValue(purpose)
+        userChild.child("email").setValue(email)
     }
     
     @IBAction func didAlreadySignup(_ sender: Any) {
